5c9faf5b05f19496daa604586ce0612034e6df09
Jeffrey Camealy <jeffrey@orainteractive.com>
2013-07-17 12:59:53 -0500

    Initial Commit

---
+++ FieldAutoScrollingScrollViewTests/FieldAutoScrollingScrollViewTests.h
@@ -0,0 +1,13 @@
+//
+//  FieldAutoScrollingScrollViewTests.h
+//  FieldAutoScrollingScrollViewTests
+//
+//  Created by Jeffrey Camealy on 7/17/13.
+//  Copyright (c) 2013 bearMountain. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface FieldAutoScrollingScrollViewTests : SenTestCase
+
+@end
---
+++ FieldAutoScrollingScrollViewTests/FieldAutoScrollingScrollViewTests.m
@@ -0,0 +1,32 @@
+//
+//  FieldAutoScrollingScrollViewTests.m
+//  FieldAutoScrollingScrollViewTests
+//
+//  Created by Jeffrey Camealy on 7/17/13.
+//  Copyright (c) 2013 bearMountain. All rights reserved.
+//
+
+#import "FieldAutoScrollingScrollViewTests.h"
+
+@implementation FieldAutoScrollingScrollViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in FieldAutoScrollingScrollViewTests");
+}
+
+@end
